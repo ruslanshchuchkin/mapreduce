e227abb5c116123bc2dd612d4f9b3fab4d6c8819
ruslanshchuchkin <ruslan.shchuchkin@gmail.com>
2020-06-10 10:54:27 +0200

    exercise 6, mapper3 reducer 2

--- reducer2.py
+++ reducer2.py
@@ -21,7 +21,7 @@ import sys
 
 # Sum of all sales (values) is initialized with zero, we just started
 count_of_values = 0
-
+sum_of_values = 0
 # Previous key is initialized with None, we just started
 previous_key = None
 
@@ -48,15 +48,17 @@ for line in sys.stdin:
         sys.stdout.write("{0}\t{1}\n".format(previous_key, count_of_values))
         # Sum of sales starts again with 0
         count_of_values = 0
+        sum_of_values = 0
 
     # Add the value to the total sales
     # a += b is the same as a = a + b
     # the float function transforms the value
     # to a float data type (like decimal)
     count_of_values += 1
+    sum_of_values += float(value)
     # the previous key for the next iteration is the current key of the this iteration 
     previous_key = key
 
 # write the last result to stdout
 if count_of_values > 114:
-    sys.stdout.write("{0}\t{1}\n".format(previous_key, count_of_values))
+    sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values / count_of_values))
